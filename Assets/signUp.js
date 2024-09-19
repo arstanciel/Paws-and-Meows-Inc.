1406763f5505b36771e60570081c5f0dcffa3fc1
Edward Ofori <tetraed.llc@gmail.com>
2024-09-19 08:58:49 -0400

    Added function to sign up form

--- Assets/signUp.js
+++ Assets/signUp.js
@@ -1,2 +1,32 @@
 const signupForm = document.querySelector ('form');
+const resultContainer = document.getElementById('result');
 console.log(signupForm);
+function signupSubmission (event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById('fullname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+
+localStorage.setItem('fullName', fullName);
+localStorage.setItem('email', email);
+localStorage.setItem('password', password);
+
+signupForm.reset();
+
+resultContainer.textContent = 'Account Successfully Created!';
+}
+signupForm.addEventListener('submit', signupSubmission)
+console.log(signupForm);
+
+window.onload = function() {
+    const storedFullName = localStorage.getItem('fullName');
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+
+    if (storedFullName && storedEmail && storedPassword) {
+        resultContainer.textContent = `Welcome back ${storedFullName}`
+    }
+    
+}
